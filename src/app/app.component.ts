6637c8c0b5377981ea95952b85e59557dda58f55
Hitesh Chavda <hiteshc@cybercom.in>
2021-11-17 14:43:55 +0530

    first init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, Directive, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+@Directive({selector: 'app-my-component'})
+export class Pane {
+  @Input() id!: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // template: `<div #child1>First Child</div>
+  //   <div>Second Child</div> 
+  //   <app-my-component id="1"></app-my-component>`,
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'v13';
+export class AppComponent implements OnInit {
+  title = 'Angular v13';
+  @ViewChild('child1') firstChild!: ElementRef;
+  //  learn about @ViewChild directory
+  //syntax :-- @ViewChild(selector,{read:ReferenceType}) fieldName;
+  constructor(private element: ElementRef) {}
+
+  ngOnInit(): void {
+    console.log('Element Ref::', this.element.nativeElement);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      MyComponentComponent
+   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' })
   ],
   providers: [],
   bootstrap: [AppComponent]
